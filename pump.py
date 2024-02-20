9ab1286c3d223a5b836877a00c3591dbafaab2f2
Ahmed Begga <ahmedbegga@MacBook-Air-de-Ahmed.local>
2024-02-20 12:18:15 +0100

    Updated all

--- pump.py
+++ pump.py
@@ -1,18 +1,17 @@
 import torch
 # Trace of a tensor [1,k,k]
 def _rank3_trace(x):
-    return torch.einsum('ij->i', x.to_dense())
+    return torch.einsum('ij->i', x)
 def pump(s, adj): 
     k = s.size(-1)
     s = torch.tanh(s) # torch.Size([20, N, k]) One k for each N of each graph
-    print(torch.matmul(s.T,adj), s)
-    print(torch.matmul(torch.matmul(s.T,adj), s))
-    CT_num = _rank3_trace(torch.matmul(torch.matmul(s.t(),adj), s)) # Tr(S^T A S) 
+    
+    CT_num = _rank3_trace(torch.matmul(torch.matmul(s.t(),adj.to_dense()), s)) # Tr(S^T A S) 
     # Degree sparse
     degrees = adj.sum(dim=1).coalesce().values()
     deg = torch.diag(degrees)
     #deg = torch.sparse_coo_tensor(adj.coalesce().indices(), degrees, size=(adj.size(0), adj.size(0)), dtype=s.dtype, device=s.device)
-    CT_den = _rank3_trace(torch.matmul(torch.matmul(s.t(), deg), s))  # Tr(S^T D S) 
+    CT_den = _rank3_trace(torch.matmul(torch.matmul(s.t(), deg.to_dense()), s))  # Tr(S^T D S) 
     # Mask with adjacency if proceeds 
     CT_loss = -(CT_num / CT_den) # Tr(S^T A S) / Tr(S^T D S)
     CT_loss = torch.mean(CT_loss)
